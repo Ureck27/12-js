ca728c9610e3290fe8eb7fe4672f861f820e91a9
unknown <Aourik rachid>
2025-11-29 18:10:38 +0100

    nothing

--- Asynch/file.js
+++ Asynch/file.js
@@ -2,3 +2,17 @@ let x = 5;
 let y = 6;
 let z = x + y;
 console.log(z)
+
+let text = "Unknown";
+let age = 18;
+if (age >= 18) {
+  text = "Adult";
+} else {
+  text = "Minor";
+}
+console.log(text)
+
+
+for (let i = 0; i < 5; i++) {
+ console.log( "The number is " + i )
+}
